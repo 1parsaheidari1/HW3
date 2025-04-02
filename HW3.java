adf56754adc846b0ed3a03d7ea0a42b36cc33ed0
Parsa Heidari <147088673+1parsaheidari1@users.noreply.github.com>
2025-04-02 11:26:44 +0330

    Update HW3.java

--- HW3.java
+++ HW3.java
@@ -9,25 +9,25 @@ public class StudentAccount {
 
     public boolean validatePassword(String password) {
         if (password.length() < 8 || password.length() < 32) {
-            return false;
+            System.out.println("The password is invalid.");
         }
 
         if (!password.matches(".*[A-Z].*")) {
-            return false;
+            System.out.println("The password is invalid.");
         }
 
         if (!password.matches(".*\\d.*")) {
-            return false;
+            System.out.println("The password is invalid.");
         }
 
         if (!password.matches(".*[!@#$%^&*()\\-_=+\\[\\]{};:'\",.<>?/\\\\|`~].*")) {
-            return false;
+            System.out.println("The password is invalid.");
         }
 
         if (password.matches(".*[\\s].*")) {
-            return false;
+            System.out.println("The password is invalid.");
         }
 
-        return true;
+        System.out.println("The password is valid.");
     }
 }
